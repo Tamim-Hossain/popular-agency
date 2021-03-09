8b089d52e12f75858bdd8aa34bb924d7be24a5e4
tamim-hossain <tamim.cloud@gmail.com>
2021-03-09 09:15:27 +0600

    home section almost done

---
+++ src/components/auth/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAdy67-5_FtV25Xp7b7sc-y8sC5Zo9L1OQ",
+	authDomain: "popular-agency.firebaseapp.com",
+	projectId: "popular-agency",
+	storageBucket: "popular-agency.appspot.com",
+	messagingSenderId: "866722772223",
+	appId: "1:866722772223:web:0dcebb367105e8119488ca",
+	measurementId: "G-4F7FBEJ7DD",
+};
+
+export default firebaseConfig;
--- src/components/home/Feedback.jsx
+++ src/components/home/Feedback.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 
 const Feedback = () => {
 	const [feedback, setFeedback] = useState([]);
@@ -11,23 +11,21 @@ const Feedback = () => {
 	}, [feedback]);
 
 	return (
-		<Container className="py-5">
-			<h2 className="text-center font-weight-bold text-success">Clients Feedback</h2>
+		<Container className="py-5 mt-5">
+			<h2 className="text-center font-weight-bold text-success mb-5">Clients Feedback</h2>
 			<Row>
 				{feedback.map((review) => (
 					<Col md={4}>
-						<div>
-							<div>
-								<img className='img-fluid w-25' src={review.userPhoto} alt="" />
+						<Card className="p-4">
+							<div className="d-flex justify-content-start">
+								<img className="w-25 img-fluid rounded-circle" src={review.userPhoto} alt="" />
+								<div className="ml-4">
+									<h3>{review.name}</h3>
+									<h5>{review.company}</h5>
+								</div>
 							</div>
-							<div>
-								<h6>{review.name}</h6>
-								<p>{review.company}</p>
-							</div>
-							<div>
-								<p>{review.description}</p>
-							</div>
-						</div>
+							<p className="text-secondary mt-3">{review.description}</p>
+						</Card>
 					</Col>
 				))}
 			</Row>
